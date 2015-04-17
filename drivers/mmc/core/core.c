be8784e41a08bbf90f2ab3cdd64604025f3f6e39
Christian Oder <myself5@carbonrom.org>
2015-04-17 18:05:25 +0200

    Revert "mmc: decrease wakelock timeout"
    This reverts commit 193a26df220859e41b6a1f13ec06def57e1215c8.

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -3308,7 +3308,7 @@ void mmc_rescan(struct work_struct *work)
 	mmc_rpm_release(host, &host->class_dev);
  out:
 	if (extend_wakelock)
-		wake_lock_timeout(&host->detect_wake_lock, HZ / 4);
+		wake_lock_timeout(&host->detect_wake_lock, HZ / 2);
 
 	if (host->caps & MMC_CAP_NEEDS_POLL)
 		mmc_schedule_delayed_work(&host->detect, HZ);
